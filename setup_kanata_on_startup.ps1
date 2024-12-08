0abbba0bcfdf757c42bdbb73db773446848f7f0e
Bachourian Rafael <bachourian@gmail.com>
2024-12-08 15:13:46 +0100

    add kanata mad

--- setup_kanata_on_startup.ps1
+++ setup_kanata_on_startup.ps1
@@ -1,12 +1,12 @@
 $StartupPath="HKCU:\Software\Microsoft\Windows\CurrentVersion\Run"
 $ProgramName="Kanata"
 # change the executable path
-$KanataPath="C:\Program Files\Kanata\kanata_gui.exe"
+$KanataPath="C:\Portable\Kanata\kanata_gui.exe"
 # change the config path
-$KanataConfigPath="C:\Program Files\Kanata\"
+$KanataConfigPath="$env:appdata\kanata-config\"
 $KanataConfigName="kanata.kbd"
+$KanataMadConfigName="kanata-mad.kbd"
 
 # C:\Windows\system32\conhost.exe --headless 
-$StartupCommand="`"$KanataPath`" --cfg `"$KanataConfigPath$KanataConfigName`""
-
+$StartupCommand="`"$KanataPath`" --cfg `"$KanataConfigPath$KanataMadConfigName`" --cfg `"$KanataConfigPath$KanataConfigName`""
 Set-ItemProperty -LiteralPath "$StartupPath" -Name "$ProgramName" -Value "$StartupCommand" 
